84813b5a1b6b8ad5e475c90c8f5d6e7cd4356fed
ParichayPai <parichaypai393@gmail.com>
2020-12-10 01:35:49 +0530

    update

--- src/app.js
+++ src/app.js
@@ -43,8 +43,23 @@ app.post("/mario", (req, res) => {
     })
 });
 
+// app.patch('/mario/:id',(req,res)=>{
+//     const id = req.params.id;
+//     const updates = req.body;
+//     console.log(updates)
+//     marioModel.findByIdAndUpdate(id,updates,{new:true})
+    
+//     .then((result)=>{
+//         res.status(200).json(result);
+//     })
+//     .catch((error)=>{
+//         res.status(400).json({"message": error.message});
+//     })
+
+// })
+
 app.patch("/mario/:id", (req, res) => {
-    marioModel.findById(req.params.id, req.body, {new:true})
+    marioModel.findByIdAndUpdate(req.params.id, req.body, {new:true})
     .then(data => res.status(200).json(data))
     .catch(err => res.status(400).json({"message" : err.message}))
 })
